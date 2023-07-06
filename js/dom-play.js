48e29a157bc26bc2538480dd0764556d699ea2a2
bryangomezcarranza <bryangomezcarranza@gmail.com>
2023-07-06 10:23:46 -0700

    Updates

--- js/dom-play.js
+++ js/dom-play.js
@@ -15,3 +15,46 @@
 */ 
 
 // document.querySelector(".hamlet").style.backgroundColor = "yellow";
+
+// This refers to the cunftion 
+function highlight(el) {
+
+    // Ternanry example 
+    // let currentColor = el.style.backgroundColor == 'white'
+    // return (currentColor ? el.style.backgroundColor ='orange' : el.style.backgroundColor ='white');
+    
+    if(el.style.backgroundColor == 'white') {
+        el.style.backgroundColor='orange';  
+    } else {
+        el.style.backgroundColor='white';  
+    }
+}
+
+function myAlert() {
+    alert("Im clicked!")
+}
+
+function highlightActor(actor) {
+   // alert("Hi, I'm " + actor);
+   for(const mySpan of spans) {
+        if(actor == mySpan.dataset.actor) {
+            mySpan.style.backgroundColor = 'orange';
+        } else {
+            mySpan.style.backgroundColor = 'white';
+        }
+   }
+}
+
+
+const spans = document.querySelectorAll("#play span");
+console.log(spans)
+
+// works like a ForEach on swift ui 
+for(const mySpan of spans) {
+    // Listener
+    //mySpan.addEventListener("click", myAlert);
+    mySpan.addEventListener("click", function(ev) {
+        highlightActor(mySpan.dataset.actor);
+    });
+   // alert(mySpan.dataset.actor);
+}
